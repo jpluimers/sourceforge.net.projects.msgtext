2851559af4e64a5b8cd24627e642b71851f211d9
hartwigthomas <hartwigthomas@2002a3e2-cbc3-451b-bbe5-1bdb0bbc1670>
2011-02-10 13:47:06 +0000

    Release 2.05

--- Transfer/mtbin.cmd
+++ Transfer/mtbin.cmd
@@ -16,7 +16,7 @@ if "%~1"=="-H" goto help
 if "%~1"=="/h" goto help
 chcp 1252
 
-set version=2_04
+set version=2_05
 
 rem --------------------------------------------------------------------
 rem execution directory from which cmd is called
